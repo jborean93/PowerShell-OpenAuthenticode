de74fd8e9a7079f5a3724183edd9704fe4839413
Jordan Borean <jborean93@gmail.com>
2023-03-25 18:33:32 +1000

    Use test binary instead of actual dll (#5)

--- tests/PEBinary.Tests.ps1
+++ tests/PEBinary.Tests.ps1
@@ -15,23 +15,22 @@ Describe "PE Binary Authenticode" {
     }
 
     BeforeEach {
-        $dllPath = Join-Path temp: test.dll
-        Copy-Item -LiteralPath ([OpenAuthenticode.SignatureHelper].Assembly.Location) -Destination $dllPath
-        $dllPath = (Get-Item -LiteralPath $dllPath).FullName
-        Clear-OpenAuthenticodeSignature -LiteralPath $dllPath
+        $exePath = Join-Path temp: test.exe
+        $sourceExe = Join-Path $PSScriptRoot data test.exe
+        Copy-Item -LiteralPath $sourceExe -Destination $exePath
+        $exePath = (Get-Item -LiteralPath $exePath).FullName
+        Clear-OpenAuthenticodeSignature -LiteralPath $exePath
     }
 
     AfterEach {
-        Remove-Item -LiteralPath $dllPath -Force
+        Remove-Item -LiteralPath $exePath -Force
     }
 
     It "Gets error in PE binary without signature" {
-        $filePath = [OpenAuthenticode.SignatureHelper].Assembly.Location
-
-        $actual = Get-OpenAuthenticodeSignature -path $filePath -ErrorAction SilentlyContinue -ErrorVariable err
+        $actual = Get-OpenAuthenticodeSignature -path $sourceExe -ErrorAction SilentlyContinue -ErrorVariable err
         $actual | Should -BeNullOrEmpty
         $err.Count | Should -Be 1
-        [string]$err[0] | Should -Be "File '$filePath' does not contain an authenticode signature"
+        [string]$err[0] | Should -Be "File '$sourceExe' does not contain an authenticode signature"
     }
 
     It "Gets multiple signatures from PE with 2 hash algorithms" {
@@ -54,28 +53,28 @@ Describe "PE Binary Authenticode" {
 
     It "Signs a dll with the default hash" {
         $setParams = @{
-            Path = $dllPath
+            Path = $exePath
             Certificate = $cert
         }
         $res = Set-OpenAuthenticodeSignature @setParams
         $res | Should -BeNullOrEmpty
 
-        $actual = Get-OpenAuthenticodeSignature -Path $dllPath @trustParams
+        $actual = Get-OpenAuthenticodeSignature -Path $exePath @trustParams
         $actual | Should -BeOfType ([System.Security.Cryptography.Pkcs.SignedCms])
-        $actual.Path | Should -Be $dllPath
+        $actual.Path | Should -Be $exePath
         $actual.HashAlgorithm | Should -Be SHA256
         $actual.TimeStampInfo | Should -BeNullOrEmpty
         $actual.Certificate.Thumbprint | Should -Be $cert.Thumbprint
 
         If (Get-Command -Name Get-AuthenticodeSignature -ErrorAction Ignore) {
-            $actual = Get-AuthenticodeSignature -FilePath $dllPath
+            $actual = Get-AuthenticodeSignature -FilePath $exePath
             $actual.Status | Should -Not -Be HashMismatch
         }
     }
 
     It "Signs and adds a signature" {
         $setParams = @{
-            Path = $dllPath
+            Path = $exePath
             Certificate = $cert
         }
         Add-OpenAuthenticodeSignature @setParams
@@ -83,7 +82,7 @@ Describe "PE Binary Authenticode" {
         $actual = Add-OpenAuthenticodeSignature @setParams -HashAlgorithm SHA384
         $actual | Should -BeNullOrEmpty
 
-        $actual = Get-OpenAuthenticodeSignature -Path $dllPath @trustParams
+        $actual = Get-OpenAuthenticodeSignature -Path $exePath @trustParams
         $actual.Count | Should -Be 2
         $actual[0].HashAlgorithm | Should -Be SHA256
         $actual[0].Certificate.Thumbprint | Should -Be $cert.Thumbprint
@@ -92,12 +91,12 @@ Describe "PE Binary Authenticode" {
 
         $actual = Add-OpenAuthenticodeSignature @setParams -HashAlgorithm SHA512 -PassThru
         $actual | Should -BeOfType ([System.Security.Cryptography.Pkcs.SignedCms])
-        $actual.Path | Should -Be $dllPath
+        $actual.Path | Should -Be $exePath
         $actual.HashAlgorithm | Should -Be SHA512
         $actual.TimeStampInfo | Should -BeNullOrEmpty
         $actual.Certificate.Thumbprint | Should -Be $cert.Thumbprint
 
-        $actual = Get-OpenAuthenticodeSignature -Path $dllPath @trustParams
+        $actual = Get-OpenAuthenticodeSignature -Path $exePath @trustParams
         $actual.Count | Should -Be 3
         $actual[0].HashAlgorithm | Should -Be SHA256
         $actual[0].Certificate.Thumbprint | Should -Be $cert.Thumbprint
@@ -107,21 +106,21 @@ Describe "PE Binary Authenticode" {
         $actual[2].Certificate.Thumbprint | Should -Be $cert.Thumbprint
 
         If (Get-Command -Name Get-AuthenticodeSignature -ErrorAction Ignore) {
-            $actual = Get-AuthenticodeSignature -FilePath $dllPath
+            $actual = Get-AuthenticodeSignature -FilePath $exePath
             $actual.Status | Should -Not -Be HashMismatch
         }
     }
 
     It "Signs and adds a signature with a timestamp" {
         $setParams = @{
-            Path = $dllPath
+            Path = $exePath
             Certificate = $cert
             TimeStampServer = "http://timestamp.digicert.com"
         }
         Add-OpenAuthenticodeSignature @setParams
         Add-OpenAuthenticodeSignature @setParams -TimestampHashAlgorithm SHA384
 
-        $actual = Get-OpenAuthenticodeSignature -Path $dllPath @trustParams
+        $actual = Get-OpenAuthenticodeSignature -Path $exePath @trustParams
         $actual.Count | Should -Be 2
         $actual[0].HashAlgorithm | Should -Be SHA256
         $actual[0].TimeStampInfo | Should -BeOfType ([OpenAuthenticode.CounterSignature])
@@ -135,7 +134,7 @@ Describe "PE Binary Authenticode" {
         $actual[1].Certificate.Thumbprint | Should -Be $cert.Thumbprint
 
         If (Get-Command -Name Get-AuthenticodeSignature -ErrorAction Ignore) {
-            $actual = Get-AuthenticodeSignature -FilePath $dllPath
+            $actual = Get-AuthenticodeSignature -FilePath $exePath
             $actual.Status | Should -Not -Be HashMismatch
         }
     }
@@ -143,47 +142,47 @@ Describe "PE Binary Authenticode" {
 
     It "Clears signed dll" {
         $setParams = @{
-            Path = $dllPath
+            Path = $exePath
             Certificate = $cert
         }
         Set-OpenAuthenticodeSignature @setParams
 
-        Clear-OpenAuthenticodeSignature -Path $dllPath
+        Clear-OpenAuthenticodeSignature -Path $exePath
 
-        $actual = Get-OpenAuthenticodeSignature -Path $dllPath @trustParams -ErrorAction SilentlyContinue -ErrorVariable err
+        $actual = Get-OpenAuthenticodeSignature -Path $exePath @trustParams -ErrorAction SilentlyContinue -ErrorVariable err
         $actual | Should -BeNullOrEmpty
         $err.Count | Should -Be 1
-        [string]$err[0] | Should -Be "File '$dllPath' does not contain an authenticode signature"
+        [string]$err[0] | Should -Be "File '$exePath' does not contain an authenticode signature"
     }
 
     It "Clears signed dll in -WhatIf" {
         $setParams = @{
-            Path = $dllPath
+            Path = $exePath
             Certificate = $cert
         }
         Set-OpenAuthenticodeSignature @setParams
 
-        Clear-OpenAuthenticodeSignature -Path $dllPath -WhatIf
+        Clear-OpenAuthenticodeSignature -Path $exePath -WhatIf
 
-        $actual = Get-OpenAuthenticodeSignature -Path $dllPath @trustParams
+        $actual = Get-OpenAuthenticodeSignature -Path $exePath @trustParams
         $actual.HashAlgorithm | Should -Be SHA256
         $actual.Certificates.Thumbprint | Should -Be $cert.Thumbprint
     }
 
     It "Clears unsigned dll without errors" {
-        Clear-OpenAuthenticodeSignature -Path $dllPath
+        Clear-OpenAuthenticodeSignature -Path $exePath
 
-        $actual = Get-OpenAuthenticodeSignature -Path $dllPath @trustParams -ErrorAction SilentlyContinue -ErrorVariable err
+        $actual = Get-OpenAuthenticodeSignature -Path $exePath @trustParams -ErrorAction SilentlyContinue -ErrorVariable err
         $actual | Should -BeNullOrEmpty
         $err.Count | Should -Be 1
-        [string]$err[0] | Should -Be "File '$dllPath' does not contain an authenticode signature"
+        [string]$err[0] | Should -Be "File '$exePath' does not contain an authenticode signature"
     }
 
     It "Write error on unsigned dll" {
-        $actual = Get-OpenAuthenticodeSignature -Path $dllPath @trustParams -ErrorAction SilentlyContinue -ErrorVariable err
+        $actual = Get-OpenAuthenticodeSignature -Path $exePath @trustParams -ErrorAction SilentlyContinue -ErrorVariable err
         $actual | Should -BeNullOrEmpty
         $err.Count | Should -Be 1
-        [string]$err[0] | Should -Be "File '$dllPath' does not contain an authenticode signature"
+        [string]$err[0] | Should -Be "File '$exePath' does not contain an authenticode signature"
     }
 
     It "Signs a dll with hash <Name>" -TestCases @(
@@ -195,29 +194,29 @@ Describe "PE Binary Authenticode" {
         param ($Name)
 
         $setParams = @{
-            Path = $dllPath
+            Path = $exePath
             Certificate = $cert
             HashAlgorithm = $Name
         }
         $res = Set-OpenAuthenticodeSignature @setParams
         $res | Should -BeNullOrEmpty
 
-        $actual = Get-OpenAuthenticodeSignature -Path $dllPath @trustParams
+        $actual = Get-OpenAuthenticodeSignature -Path $exePath @trustParams
         $actual | Should -BeOfType ([System.Security.Cryptography.Pkcs.SignedCms])
-        $actual.Path | Should -Be $dllPath
+        $actual.Path | Should -Be $exePath
         $actual.HashAlgorithm | Should -Be $Name
         $actual.TimeStampInfo | Should -BeNullOrEmpty
         $actual.Certificate.Thumbprint | Should -Be $cert.Thumbprint
 
         If (Get-Command -Name Get-AuthenticodeSignature -ErrorAction Ignore) {
-            $actual = Get-AuthenticodeSignature -FilePath $dllPath
+            $actual = Get-AuthenticodeSignature -FilePath $exePath
             $actual.Status | Should -Not -Be HashMismatch
         }
     }
 
     It "Signed with timestamp" {
         $setParams = @{
-            Path = $dllPath
+            Path = $exePath
             Certificate = $cert
             HashAlgorithm = "SHA384"
             TimeStampServer = "http://timestamp.digicert.com"
@@ -225,9 +224,9 @@ Describe "PE Binary Authenticode" {
         $res = Set-OpenAuthenticodeSignature @setParams
         $res | Should -BeNullOrEmpty
 
-        $actual = Get-OpenAuthenticodeSignature -Path $dllPath @trustParams
+        $actual = Get-OpenAuthenticodeSignature -Path $exePath @trustParams
         $actual | Should -BeOfType ([System.Security.Cryptography.Pkcs.SignedCms])
-        $actual.Path | Should -Be $dllPath
+        $actual.Path | Should -Be $exePath
         $actual.HashAlgorithm | Should -Be SHA384
         $actual.TimeStampInfo | Should -BeOfType ([OpenAuthenticode.CounterSignature])
         $actual.TimeStampInfo.Certificate | Should -Not -BeNullOrEmpty
@@ -237,7 +236,7 @@ Describe "PE Binary Authenticode" {
         $actual.Certificate.Thumbprint | Should -Be $cert.Thumbprint
 
         If (Get-Command -Name Get-AuthenticodeSignature -ErrorAction Ignore) {
-            $actual = Get-AuthenticodeSignature -FilePath $dllPath
+            $actual = Get-AuthenticodeSignature -FilePath $exePath
             $actual.Status | Should -Not -Be HashMismatch
         }
     }
@@ -250,7 +249,7 @@ Describe "PE Binary Authenticode" {
         param ($Name)
 
         $setParams = @{
-            Path = $dllPath
+            Path = $exePath
             Certificate = $cert
             TimeStampServer = "http://timestamp.digicert.com"
             TimeStampHashAlgorithm = $Name
@@ -258,9 +257,9 @@ Describe "PE Binary Authenticode" {
         $res = Set-OpenAuthenticodeSignature @setParams
         $res | Should -BeNullOrEmpty
 
-        $actual = Get-OpenAuthenticodeSignature -Path $dllPath @trustParams
+        $actual = Get-OpenAuthenticodeSignature -Path $exePath @trustParams
         $actual | Should -BeOfType ([System.Security.Cryptography.Pkcs.SignedCms])
-        $actual.Path | Should -Be $dllPath
+        $actual.Path | Should -Be $exePath
         $actual.HashAlgorithm | Should -Be SHA256
         $actual.TimeStampInfo | Should -BeOfType ([OpenAuthenticode.CounterSignature])
         $actual.TimeStampInfo.Certificate | Should -Not -BeNullOrEmpty
@@ -270,13 +269,13 @@ Describe "PE Binary Authenticode" {
         $actual.Certificate.Thumbprint | Should -Be $cert.Thumbprint
 
         If (Get-Command -Name Get-AuthenticodeSignature -ErrorAction Ignore) {
-            $actual = Get-AuthenticodeSignature -FilePath $dllPath
+            $actual = Get-AuthenticodeSignature -FilePath $exePath
             $actual.Status | Should -Not -Be HashMismatch
         }
     }
 
     It "Signs PE binary with extra data on the end" {
-        $fs = [System.IO.File]::Open($dllPath, "Open", "Write")
+        $fs = [System.IO.File]::Open($exePath, "Open", "Write")
         try {
             $null = $fs.Seek(0, 'End')
             $fs.WriteByte(1)
@@ -288,41 +287,41 @@ Describe "PE Binary Authenticode" {
         }
 
         $setParams = @{
-            Path = $dllPath
+            Path = $exePath
             Certificate = $cert
         }
         Set-OpenAuthenticodeSignature @setParams
 
-        $actual = Get-OpenAuthenticodeSignature -Path $dllPath @trustParams
+        $actual = Get-OpenAuthenticodeSignature -Path $exePath @trustParams
         $actual | Should -BeOfType ([System.Security.Cryptography.Pkcs.SignedCms])
-        $actual.Path | Should -Be $dllPath
+        $actual.Path | Should -Be $exePath
         $actual.HashAlgorithm | Should -Be SHA256
         $actual.TimeStampInfo | Should -BeNullOrEmpty
         $actual.Certificate.Thumbprint | Should -Be $cert.Thumbprint
 
         If (Get-Command -Name Get-AuthenticodeSignature -ErrorAction Ignore) {
-            $actual = Get-AuthenticodeSignature -FilePath $dllPath
+            $actual = Get-AuthenticodeSignature -FilePath $exePath
             $actual.Status | Should -Not -Be HashMismatch
         }
 
-        Clear-OpenAuthenticodeSignature -Path $dllPath
+        Clear-OpenAuthenticodeSignature -Path $exePath
 
-        $actual = Get-OpenAuthenticodeSignature -Path $dllPath @trustParams -ErrorAction SilentlyContinue -ErrorVariable err
+        $actual = Get-OpenAuthenticodeSignature -Path $exePath @trustParams -ErrorAction SilentlyContinue -ErrorVariable err
         $actual | Should -BeNullOrEmpty
         $err.Count | Should -Be 1
-        [string]$err[0] | Should -Be "File '$dllPath' does not contain an authenticode signature"
+        [string]$err[0] | Should -Be "File '$exePath' does not contain an authenticode signature"
 
         Set-OpenAuthenticodeSignature @setParams
 
-        $actual = Get-OpenAuthenticodeSignature -Path $dllPath @trustParams
+        $actual = Get-OpenAuthenticodeSignature -Path $exePath @trustParams
         $actual | Should -BeOfType ([System.Security.Cryptography.Pkcs.SignedCms])
-        $actual.Path | Should -Be $dllPath
+        $actual.Path | Should -Be $exePath
         $actual.HashAlgorithm | Should -Be SHA256
         $actual.TimeStampInfo | Should -BeNullOrEmpty
         $actual.Certificate.Thumbprint | Should -Be $cert.Thumbprint
 
         If (Get-Command -Name Get-AuthenticodeSignature -ErrorAction Ignore) {
-            $actual = Get-AuthenticodeSignature -FilePath $dllPath
+            $actual = Get-AuthenticodeSignature -FilePath $exePath
             $actual.Status | Should -Not -Be HashMismatch
         }
     }
